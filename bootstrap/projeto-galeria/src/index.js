76838b9e21e3ba5a34a1510a68e785abf5fa393a
Allan Guilherme Guimarães Ribeiro <allan@navas.design>
2020-10-12 23:36:42 -0300

    Adicionando Bootstrap, Gulp, Webpack, JQuery

---
+++ bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,10 @@
+// Arquivo SASS
+import './scss/index.scss'
+
+// Dependências
+import 'jquery'
+import 'bootstrap'
+
+// Meus arquivos JavaScript
+import './js/core/includes'
+import './js/plugins/cityButtons'
