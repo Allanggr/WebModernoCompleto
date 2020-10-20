b6042c9ad5f754d75c16694eca008f4b5d64ac6e
Allan Guilherme Guimarães Ribeiro <allan@navas.design>
2020-10-19 23:59:32 -0300

    Estudando React

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,6 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const elemento = <h1>React</h1>
+// tem que ter letra maiuscula!!
+// import BomDia from './componentes/BomDia'
 
-ReactDOM.render(elemento, document.getElementById('root'))
+// import Multi, { BoaNoite} from './componentes/Multiplos'
+
+import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
+// ReactDOM.render(
+//     <div>
+//         <Multi.BoaTarde nome="Ana"/>
+//         <BoaNoite nome="Bia"/>
+//     </div>
+//     , document.getElementById('root'))
+
+// ReactDOM.render(
+//     <div>
+//         <Saudacao tipo="Bom dia" nome="João"/>
+//     </div>
+// , document.getElementById('root'))
+
+ReactDOM.render(
+    <div>
+        <Pai nome="Paulo" sobrenome="Silva">
+            {/* Como passo os componentes Filhos aqui? */}
+            <Filho nome="Pedro" />
+            <Filho nome="Paulo" />
+            <Filho nome="Carla" />
+        </Pai>
+    </div>
+, document.getElementById('root'))
